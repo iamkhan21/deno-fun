755d2bf7ac92b941aba4abb9b3059118c899cb08
Ivan Khanevich <mail@iamkhan.tech>
2022-02-01 20:38:33 +0300

    init

---
+++ src/components/Comments.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from '../nano.ts';
+
+export class Comments extends Component {
+	render() {
+		return (
+			<ul>
+				{this.props.comments.map((comment: any) => {
+					return <li>{comment}</li>;
+				})}
+			</ul>
+		);
+	}
+}
